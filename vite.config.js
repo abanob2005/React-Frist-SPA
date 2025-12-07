7c6b47de7d031087a6a128df517e94dc66f70b72
Abanob Hanna <abanobs888@gmail.com>
2025-12-08 00:56:44 +0200

    2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// استبدل 'YOUR_REPO_NAME' بالاسم الفعلي لمستودعك (مثال: '/Frist-React/')
+const base_path = 'https://github.com/abanob2005/React-Frist-SPA/'; 
+
 export default defineConfig({
   plugins: [react()],
+  base: base_path, // <--- هذا هو التعديل المطلوب
 })
